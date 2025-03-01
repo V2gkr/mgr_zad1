41865fdea6e5155ec8f421dae891efaae78222e8
V2gkr <vova.grisenko.vg@gmail.com>
2025-03-01 08:32:43 +0100

    Updated rcmc & azimuth compression

--- zad1.m
+++ zad1.m
@@ -1,118 +1,150 @@
 close all;
 clear;clc;
-
 %% signal parameters
-f_sin=1e9;%1GHz
-c=physconst('LightSpeed');
-lambda=c/f_sin;
-A=1;%1V unused
-pulseDuration=10e-6; %100us
-B=2e6;%bandwidth in hz
-%modulation index
-mi=B/pulseDuration;
-%max range
-Rmax=100e3;
-%pulse range time
-PRI=0.1e-3;%1 ms
-%total time of simulation
-TotalTime=10e-3;%10ms
-%sample time & frequency
-f_sample=4e6;%50MHz
-t_sample=1/f_sample;
+f_sin = 1e9;%frequency of modulated signal
+c = physconst('LightSpeed');
+lambda = c/f_sin; %wavelenght
+pulseDuration = 10e-6;%time of signal transmission
+B = 2e6; %bandwidth            
+mi=B/pulseDuration; %modulation coefficient
+PRI = 1/100;%frequency of azimuth sampling
+PRF=1/PRI;
+TotalTime = 0.5;         % total time of meas , unused
+f_sample = 60e6;        % range sample frequency
+
+% param calculation 
+% number of pulses (azimuth samples)
+N_pulses = floor(TotalTime/PRI);  
+%sample time 
+t_sample = 1/f_sample;
 %range resolution
-dR=c/(2*f_sample);
-%% point parameter - location & RCS
-object_location=[100,100,0]; %x,y,z in km
-object_RCS=0.5;
-%satelite parameters - location & movement speed
-satelite_location=[0,0,100e3]; %x,y,z in km
-V_satelite=[0,1000,0]; %1km/s
-%satellite is moving in the direction of y axis 
+dR = c/(2*f_sample);
 
-%% signal generation
-%time vector for whole time, unused for now
-t=0:t_sample:TotalTime;
-%time vector for a single pulse
-timp=0:t_sample:(pulseDuration-t_sample);
+%% geometry params
+%параметры спутника - расстояние , скорость 
+satelite_location = [0, 0, 100e3];  
+V_satelite = [0, 150, 0];     
+%параметры цели 
+object_location = [1000,5000, 0];   
+%max distance from radar to target 
+Rmax=120e3; % previously it was 30e3 , but its not enough if satellite altitude is 100km
 
-%signal 
-%t_signal=0:1/f_sample:pulseDuration;
-%signal=A*sin(2*pi*f_sin*t_signal);
-%signal is then used in a loop to simulate echo
-signal=exp(2i*pi*(mi*timp-mi*pulseDuration).*timp/2);
-%imp_time=0:PRI:TotalTime;
+%closest range
+R0_nonorm=[object_location(1)-satelite_location(1),0,object_location(3)-satelite_location(3)];
+R0=norm(R0_nonorm);
+%num of range samples
+N_range = floor(Rmax/dR);
+% matrix for initial raw data N_pulses columns and N_range rows
+%матрица для данных - Колво импульсов по вертикали(колонки) и расстояние по
+%горизонтали (ряды)
+raw_data = zeros(N_range, N_pulses);
 
-%% calculation of values needed to future loop
-% number of samples
-N=TotalTime/PRI;
-%calculating num of samples per period
-N_sample=floor(Rmax/dR);
-%to save the number of samples in one pulse
-sample_per_pulse=N_sample; 
-%scaling to number of measurements
-N_sample=N_sample*N;
-tabdelt=zeros(1,N_sample);
-%satellites travaled distance for 10ms - 10km
-satellite_path=TotalTime*V_satelite;
-%% echo , freq and phase calculation
-f_doppler=zeros(1,N);
-s=zeros(1,N);
-for i=1:(N)
-    %distance between satellite and object
-    distance=object_location-(satelite_location+V_satelite*i*PRI);
-    %distance between satellite and object in i-th moment
-    d_norm=norm(distance);
-    d_i=distance/d_norm;
-    %recalculation of sample number for each norm distance
-    nrprob=floor(d_norm/dR);
-    %relative velocity in i-th moment
-    V_rel=V_satelite*d_i';
-    %doppler shift in i-th moment , needed to 
-    f_doppler(i)=(2*V_rel*f_sin)/c;
-    %echo recalculation 
-    tabdelt(nrprob+(i-1)*sample_per_pulse)=tabdelt(nrprob+(i-1)*sample_per_pulse) + object_RCS*exp( -4i*pi*d_norm/lambda  +  2i*pi*f_doppler(i) );
-    temp=conv(tabdelt,signal);
-    sygnOdb=temp(1:N_sample);
+%% calculating transmitted signal
+t_chirp = 0:t_sample:(pulseDuration-t_sample);
+chirp = exp(2i*pi*(mi*t_chirp-mi*pulseDuration).*t_chirp/2);
 
-end
-%% compression
-sygnOdb_matrix=reshape(sygnOdb,sample_per_pulse,N)';
-range_compressed=zeros(N,sample_per_pulse);
-for i=1:N
-    range_compressed(i,:)=conv(sygnOdb_matrix(i,:),signal,'same');
+%% precalculation 
+% used to calculate sattelite path timings
+pulse_times = (0:N_pulses-1) * PRI;
+% calculating whole path of satellite
+satellite_positions = satelite_location + V_satelite .* pulse_times';
+satellite_positions = reshape(satellite_positions, N_pulses, 3);
+
+f_doppler=zeros(1,N_pulses);
+%%calculation per every azimuth sample
+for i = 1:N_pulses
+    % distance calculation
+    distance = object_location - satellite_positions(i,:);
+    d_norm = norm(distance);
+    % doppler shift calculation
+    V_rel = dot(V_satelite, distance/d_norm);
+    %azimuth chirp rate
+    
+    %doppler frequency
+    f_doppler(i) = (2*V_rel*f_sin)/c;
+    %calculation range cell number 
+    range_idx = floor(d_norm/dR);
+    % calculating phase 
+    phase = -4*pi*d_norm/lambda - 2*pi*f_doppler(i)*pulse_times(i);
+    raw_data(range_idx, i) = exp(1i * phase);
 end
 
-% tu nic waznego , tylko wykresy
+%% raw data
 figure;
-imagesc(abs(range_compressed));
-range_axis = (0:sample_per_pulse-1) * dR; % in meters
-azimuth_axis = (0:N-1) * PRI * 1000; % Approximate azimuth in meters
+imagesc(abs(raw_data));
+xlabel('Azimuth freq');
+ylabel('Range samples');
+title('Raw SAR data');
+colorbar;
+
+mathed_filter=conj(flipud(chirp));%flip just to turn horizontal to vertical
+
+%compression first approach
+F=fftshift(fft(chirp,N_range));
+window=hamming(N_range);
+F=F'.*window;
+R_cmp=zeros(size(raw_data));
+for i=1:N_pulses
+    R=fft(raw_data(:,i),N_range);
+    R_cmp(:,i)=R.*F;
+    R_cmp(:,i)=ifft(R.*F);
+end
+
+%% azimuth fft
+raw_az_fft=fftshift(fft(R_cmp',[],1),1);
 figure;
-imagesc(range_axis, azimuth_axis, abs(range_compressed));
+imagesc(abs(raw_az_fft));
+title('range doppler map');
+xlabel('Range samples');
+ylabel('Azimuth freq');
 
-%filtracja
-window=hamming(N);
-range_compressed=range_compressed.*window;
 
-%azimuth fft
-range_doppler_map=fftshift(fft(range_compressed,[],1),1);
 
-%azimuth ifft
-sar_image=ifft(range_doppler_map,[],1);
+%% rcmc
+delta_R = (lambda^2 * f_doppler.^2 * R0) / (8 * V_satelite(2)^2);
+delta_samples = delta_R / dR;  
+raw_RCMC= zeros(size(raw_az_fft));
+%{
+%first approach from book
+[Frg_2D, dR2D] = meshgrid(Frg,dR);
+G = exp(1j * 4 * pi * Frg_2D .* dR2D / c);
+raw_RCMC=raw_az_fft.*G;
+%}
+%another approach from book
+for i= 1:N_pulses
+  shift = delta_samples(i);
+    
+  % calcof bins to shift
+  range_bins = 1:N_range;
+  shifted_bins = range_bins - shift;
+  
+  % interpolation for shifting range samples
+  raw_RCMC(i,:) = interp1(range_bins, raw_az_fft(i,:), shifted_bins, 'linear', 0);
+end
+figure;
+imagesc(abs(raw_RCMC));
+title('rcmc');
+xlabel('Range samples');
+ylabel('Azimuth freq');
+
+%% azimuth compression 
+Kaz=(2*V_satelite(2)^2)/(lambda*R0);
+t_az=(-N_pulses/2:(N_pulses-1)/2)/PRF;
+h_az=exp(-1i*pi*Kaz*t_az.^2);
+window_azimuth = hamming(N_pulses)';
+h_az = h_az .* window_azimuth;
+for i = 1:N_range
+    raw_RCMC(:,i) = raw_RCMC(:,i) .* h_az';
+end
+%% azimuth ifft
+
+sar_image=ifft(raw_RCMC,[],2);
 figure;
 imagesc(abs(sar_image));
-%% plotting
-%plot
-%figure;
-%plot(f_doppler);
-%this is a doppler shift for each measurement . Smallest one on the 50 km , where the target is
-%figure;
-%plot(real(s));
-%hold on;
-%plot(imag(s));
-%figure;
-%plot(abs(sygnOdb),'b');
-%figure;
-%plot(real(sygnOdb),'b');
-%hold on;
-%plot(imag(sygnOdb),'r');
+xlabel('Azimuth freq');
+ylabel('Range samples');
+title('SAR data after rda');
+colorbar;
+
+
+
